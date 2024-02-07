20688d99d4b929345233fb2829e066dac38a1984
Zarrin Rahman <zarrin_r01@hotmail.co.uk>
2024-02-07 14:27:05 +0000

    added test for firstName getter

--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -1,2 +1,27 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.time.LocalDate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class BankAccountTest {
+
+    private BankAccount account;
+
+    @BeforeEach
+    public void setUp() {
+        LocalDate dateOfBirth = new LocalDate.of(1985, 1, 9);
+        account = new BankAccount("John", "Doe", dateOfBirth, 12345);
+    }
+
+    @Test
+    public void canGetFirstName() {
+        // ARRANGE
+        String expected = "John";
+        // ACT
+        String actual = account.getFirstName();
+        // ASSERT
+        assertThat(actual).isEqualTo(expected);
+
+    }
 }
